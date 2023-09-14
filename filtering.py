14ce75e732f133afae1872c28f221b308f227972
Hmic1102 <113384319+Hmic1102@users.noreply.github.com>
2023-09-14 10:59:49 -0400

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -1,27 +1,17 @@
 import numpy as np
 
-# Step 1: Calculate the softmax scores (assuming `samples` is a list of lists with the scores)
-samples = [...] # Replace with your 5000 samples with their scores
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
-
-filtered_samples = []
-
-for sample in samples:
-    # Step 2: Apply softmax and find the highest softmax score
-    softmax_scores = softmax(np.array(sample))
-    max_softmax_score = np.max(softmax_scores)
+def filtering(Prob):
     
-    # Step 3: Calculate the sum of the rest of the softmax scores
-    rest_softmax_scores_sum = np.sum(softmax_scores) - max_softmax_score
-    
-    # Step 4: Compare the highest softmax score with 50% of the sum of rest of the scores
-    if max_softmax_score < 0.5 * rest_softmax_scores_sum:
-        # Step 5: Filter out the sample
-        continue
-    
-    filtered_samples.append(sample)
+    filtered_samples = []
 
-# Now, `filtered_samples` contains the samples that passed the filtering criterion
+    for softmax in Prob:
+        max_softmax_score = np.max(softmax)
+    
+        rest_softmax_scores_sum = np.sum(softmax) - max_softmax_score
+    
+        if max_softmax_score < 0.5 * rest_softmax_scores_sum:
+            continue
+    
+        filtered_samples.append(softmax)
+
